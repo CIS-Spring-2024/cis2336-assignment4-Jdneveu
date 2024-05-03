ea1f8d6e333237a309773b83a7443da699dea952
Jdneveu <jdneveu@cougarnet.uh.edu>
2024-05-03 02:46:48 -0500

    Folders created

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000; // Choose any port
+
+// Body parser middleware to handle form data
+app.use(express.urlencoded({extended: true}));
+
+// Serve static files from the "public folder"
+app.use(express.static('public'));
+
+//GET request to serve the order form
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/order.html');
+});
+
+//POST request to handle form submission
+app.post('/submit',(req,res)=>{
+    const {item1, item2, item3} = req.body;
+
+    //Calculate total
+const total = parseInt(item1) + parseInt(item2) + parseInt(item3);
+
+//Send back total in response
+res.send('Total: $$(total)');
+})
+
+//Server listing
+app.listen(PORT,()=>{
+    console.log('Server running on http://localhost:${PORT}');
+})
